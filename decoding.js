b5cd3746dd444520fc42d5253c28c0a826ee5af5
Gitroars <501sterror@gmail.com>
2021-12-10 09:14:11 +0700

    Finish JS Practice 2

--- decoding.js
+++ decoding.js
@@ -2,18 +2,30 @@ const digitalDecipher = (eMessage,key)=>{
 const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 const number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26];
 let resultNumber = [];
-let decodedMessage = "";
-for (let i=0;i<eMessage.length;i++){
-    resultNumber.push(eMessage[i]-key[i]);
-    
-}
-console.log(resultNumber);
-for (let i=0;i<resultNumber.length;i++){
-    let numIndex = number.indexOf(resultNumber[i]);
-    let decodedAlphabet = alphabet[numIndex];
-    decodedMessage += decodedAlphabet;
-}
-console.log(decodedMessage);
-}
+let keyIndex = 0;
+let stringKey = key.toString();
 
+let decodedMessage = "";
+
+
+
+ for (let i=0;i<eMessage.length;i++){
+     
+     resultNumber.push(eMessage[i]-stringKey.charAt(keyIndex));
+     keyIndex++;
+     if(keyIndex==stringKey.length){
+         keyIndex=0;
+     }
+    
+ }
+ 
+ for (let i=0;i<resultNumber.length;i++){
+     let numIndex = number.indexOf(resultNumber[i]);
+     let decodedAlphabet = alphabet[numIndex];
+     decodedMessage += decodedAlphabet;
+ }
+ console.log(`The message from [${resultNumber}] is ${decodedMessage}`);
+}
 digitalDecipher([20, 12, 18, 30, 21], 1939);
+digitalDecipher([14, 30, 11, 1, 20, 17, 18, 18], 1990) ;
+digitalDecipher([6, 4, 1, 3, 9, 20], 100);
